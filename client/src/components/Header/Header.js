d9146c38b19116d42d614bd0afde60e0a342820c
ZhannaMelnyk <58272440+ZhannaMelnyk@users.noreply.github.com>
2020-06-02 22:32:32 +0300

    some links changed

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -6,11 +6,13 @@ const logo = require('../../assets/Bakery-logo.png')
 class Header extends React.Component {
 	render() {
 		return <header className='header'>
-			<img className='header__logo' src={logo} alt='bakery-logo' />
+			<Link to='/products'>
+				<img className='header__logo' src={logo} alt='bakery-logo' />
+			</Link>
 			<div className='header__rectangle' />
 			<nav className='header__nav'>
-				<Link to='/'>Home</Link>
-				<Link to='/lalala'>Cart</Link>
+				<Link to='/products'>Home</Link>
+				<Link to='/cart'>Cart</Link>
 			</nav>
 		</header>
 	}
